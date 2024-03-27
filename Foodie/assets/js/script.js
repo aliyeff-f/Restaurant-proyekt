d528d764428cdc7fda58d728eb2c9eb7e150d9b6
aliyeff-f <eliyevnurlan811@gmail.com>
2024-03-28 00:30:51 +0400

    main

--- Foodie/assets/js/script.js
+++ Foodie/assets/js/script.js
@@ -60,3 +60,27 @@ for (let i = 0; i < searchBoxElems.length; i++) {
     document.body.classList.toggle("active");
   });
 }
+
+const deliveryBoy = document.querySelector("[data-delivery-boy]");
+
+let deliveryBoyMove = -80;
+let lastScrollPos = 0;
+
+window.addEventListener("scroll", function () {
+
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+
+  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+    let activeScrollPos = window.scrollY;
+
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1;
+    } else {
+      deliveryBoyMove -= 1;
+    }
+
+    lastScrollPos = activeScrollPos;
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+  }
+
+});
